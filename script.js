27ef131f4f7b1f3edd53efcc7ba9056f9dcdaef8
Ansh Mahajan <anshmahajan678@gmail.com>
2026-01-14 07:16:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function makeid(l) {
   // write your code here
+	let res ="";
+	let char_list ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnoopqrstuvwxyz1234567890";
+	for(let i =0; i<l; i++){
+		res+= char_list.char_At(Math.floor(Math.random()*char_list.length
+										  )
+							   )
+	}
+	return res;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+// const l = prompt("Enter a number.");
+// alert(makeid(l));
